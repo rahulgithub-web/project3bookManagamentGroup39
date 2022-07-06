99470b6042528918c78c16aa317f51b3fe834bc9
imssiddiqui <106057264+imssiddiqui@users.noreply.github.com>
2022-07-06 13:28:02 +0530

    edit

---
+++ src/controllers/userController.js
@@ -0,0 +1,58 @@
+const usermodel = require("../models/userModel")
+
+const jwt = require('jsonwebtoken');
+const userModel = require("../models/userModel");
+
+
+const createUser = async function (req, res) {
+
+    try{
+    let userDetails = req.body 
+      //<------Checking Whether Request Body is empty or not----------->//
+    if(!userDetails ){
+        return res.status(400).send({status : false, msg : "All fields are mandatory."})
+    }
+    //<-------Creation Creation----------->//
+    
+   let userCreated = await userModel.create(userDetails)
+   res.status(201).send({ status : true , data: userCreated })
+  
+   } catch (err) {
+       return res.status(500).send({ msg: err.message })
+   }
+}
+
+
+
+
+//////////////////////login api ////////////////////////
+
+const userLogin = async function (req, res) {
+
+    try {
+        const data = req.body;
+      const { email, password, userId } = data;
+      let user = await userModel.findOne({ email, password})
+      let token = jwt.sign(
+        { 
+            userId: user._id.toString(), iat: Math.floor(Date.now() / 1000),
+      exp: Math.floor(Date.now() / 1000) + (30 * 60)
+
+        },
+      "group-39",
+);
+
+      res.header("x-api-key", token);
+    res.status(200).send({ status: true, token: token, userId: userId });
+    }
+
+    catch(err) {
+        res.status(500).send({ status: false, message: err.message })
+
+    }
+}
+
+
+
+module.exports.createUser = createUser
+module.exports.userLogin = userLogin
---
+++ src/routes/routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const bookController = require("../controllers/bookController")
+const userController = require("../controllers/userController")
+
+router.post("/register", userController.createUser)
+router.post("/login", userController.userLogin)
+
+
+
+
+
+router.all('/*', async function(req, res){
+    res.status(404).send({status: false, msg: "Page Not Found!!!"})
+})
+
+
+module.exports = router;
