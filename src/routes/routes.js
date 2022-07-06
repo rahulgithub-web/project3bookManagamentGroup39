1ffb0e9784217576c46432025dda38f561b8b48a
Raykhgan123 <105577402+raykhan123@users.noreply.github.com>
2022-07-06 12:51:10 +0530

    register

---
+++ src/routes/routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const bookController = require("../controllers/bookController")
+const userController = require("../controllers/userController")
+
+router.post("/register", userController.createUser)
+
+
+
+
+
+router.all('/*', async function(req, res){
+    res.status(404).send({status: false, msg: "Page Not Found!!!"})
+})
+
+
+module.exports = router;
